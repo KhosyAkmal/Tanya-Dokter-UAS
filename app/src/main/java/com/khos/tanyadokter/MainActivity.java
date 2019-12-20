13e5c1fa04e050bde8db8a987d931b62fee4f21c
KhosyAkmal <akmalkhosy@gmail.com>
2019-12-21 03:57:24 +0700

    add fuction Create to firebase

--- app/src/main/java/com/khos/tanyadokter/MainActivity.java
+++ app/src/main/java/com/khos/tanyadokter/MainActivity.java
@@ -2,17 +2,81 @@ package com.khos.tanyadokter;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.khos.tanyadokter.model.keluhan;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "data";
+    private DatabaseReference database;
+
+    private EditText nama, nohp, kel;
+    Button button;
+    private ProgressDialog loading;
+
+    View view;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        database = FirebaseDatabase.getInstance().getReference();
+
+        nama = findViewById(R.id.f_nama);
+        nohp = findViewById(R.id.f_nohp);
+        kel = findViewById(R.id.f_keluhan);
+    }
+
+    private void SubmitData (keluhan keluhan){
+        database.child("Data")
+                .push()
+                .setValue(keluhan)
+                .addOnSuccessListener(this, new OnSuccessListener() {
+                    @Override
+                    public void onSuccess(Object aVoid) {
+                        nama.setText("");
+                        nohp.setText("");
+                        kel.setText("");
+
+                        Toast.makeText(MainActivity.this,
+                                "Data Berhasil Ditambahkan",
+                                Toast.LENGTH_SHORT).show();
+                    }
+                });
+    }
+
+    public void handlerClikDaftarkan(View view) {
+        String _nama = nama.getText().toString();
+        String _nohp = nohp.getText().toString();
+        String _kel = kel.getText().toString();
+
+        if (_nama.equals("")) {
+            nama.setError("Silahkan Masukan Nama");
+            nama.requestFocus();
+        }  else if (_nohp.equals("")) {
+            nohp.setError("Silahkan Masukan Jumlah Barang");
+            nohp.requestFocus();
+        } else if (_kel.equals("")) {
+            kel.setError("Silahkan Masukan Harga Barang");
+            kel.requestFocus();
+        } else {
+
+            SubmitData(new keluhan(
+                    _nama.toLowerCase(),
+                    _nohp.toLowerCase(),
+                    _kel.toLowerCase()
+            ));
+        }
     }
 }
+
