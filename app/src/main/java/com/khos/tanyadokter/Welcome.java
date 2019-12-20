adb63d007c2db39578f7e20403e01f65c8eb3c1d
KhosyAkmal <akmalkhosy@gmail.com>
2019-12-21 04:06:45 +0700

    add view welcome activity

---
+++ app/src/main/java/com/khos/tanyadokter/Welcome.java
@@ -0,0 +1,21 @@
+package com.khos.tanyadokter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class Welcome extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+    }
+
+    public void loadFragmentWelcome(View view) {
+    }
+
+    public void loadFragmentList(View view) {
+    }
+}
